81fb59caddfe3aebab289cb7d9724f9407a0a460
Diego Vélez <diego.velez.dev@gmail.com>
2023-10-26 16:10:11 -0400

    Created transfer route html and css, added ls command and fetch output to page

--- src/main/kotlin/com/diego/velez/musickery/routes/TransferRoute.kt
+++ src/main/kotlin/com/diego/velez/musickery/routes/TransferRoute.kt
@@ -1,12 +1,20 @@
 package com.diego.velez.musickery.routes
 
+import com.diego.velez.musickery.android.ls
+import io.ktor.http.*
+import io.ktor.server.mustache.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun RootRoutingBuilder.transferRoute() {
     route("transfer") {
         get {
-            call.respondText("we in transfer baby!")
+//            call.respond(MustacheContent("transfer/main.html", null))
+            call.respondTemplate("transfer/main.html")
+        }
+
+        get("ls") {
+            call.respondText(ls(), ContentType.Text.Plain)
         }
     }
 }
