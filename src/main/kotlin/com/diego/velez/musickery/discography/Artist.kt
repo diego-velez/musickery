94b43c3aaae99ac52f11a42516b678c033f150df
Diego Vélez <diego.velez.dev@gmail.com>
2023-11-09 16:03:58 -0400

    Fix bug when multiple albums had the same name
    - Removed usage of Discography.albumMap because of bug with albums that
      had the same name

--- src/main/kotlin/com/diego/velez/musickery/discography/Artist.kt
+++ src/main/kotlin/com/diego/velez/musickery/discography/Artist.kt
@@ -1,20 +1,16 @@
 package com.diego.velez.musickery.discography
 
-import io.ktor.server.plugins.*
 import kotlinx.serialization.Serializable
 
-
 class Artist(var name: String) {
-
-//        private val albums = Collections.synchronizedList(mutableListOf<Album>())
     private val albums = mutableListOf<Album>()
 
     fun addAlbum(album: Album) {
         albums.add(album)
     }
 
-    fun getAlbum(name: String): Album {
-        return albums.find { it.name == name } ?: throw NotFoundException("Could not find album: $name")
+    fun getAlbum(name: String): Album? {
+        return albums.find { it.name == name }
     }
 
     fun serialized(): Serialized {
--- src/main/kotlin/com/diego/velez/musickery/discography/Discography.kt
+++ src/main/kotlin/com/diego/velez/musickery/discography/Discography.kt
@@ -14,7 +14,6 @@ object Discography {
 
     private val _allSongs = Collections.synchronizedMap(mutableMapOf<Int, Song>())
     private val artistMap = Collections.synchronizedMap(mutableMapOf<String, Artist>())
-    private val albumMap = Collections.synchronizedMap(mutableMapOf<String, Album>())
 
     // The key is each song's absolute path's hash code
     val allSongs: Map<Int, Song>
@@ -49,35 +48,22 @@ object Discography {
      */
     fun addSong(song: Song) {
         _allSongs[song.absolutePath.hashCode()] = song
-        addToAlbum(song)
-    }
 
-    /**
-     * Add [song] to its tagged [Album].
-     */
-    private fun addToAlbum(song: Song) {
-        val songAlbum = song.tags[Tag.Name.ALBUM]?.getOrDefault("Text", "N/A") ?: "N/A"
-        var album = albumMap[songAlbum]
-
-        if (album == null) {
-            album = Album(songAlbum)
-            albumMap[songAlbum] = album
-            addToArtist(song, album)
-        }
-        album.addSong(song)
-    }
-
-    /**
-     * Add [album] to the [song]'s tagged [Artist].
-     */
-    private fun addToArtist(song: Song, album: Album) {
+        // Get the artist
         val songArtist = song.tags[Tag.Name.ARTIST]?.getOrDefault("Text", "N/A") ?: "N/A"
         var artist = artistMap[songArtist]
-
         if (artist == null) {
             artist = Artist(songArtist)
             artistMap[songArtist] = artist
         }
-        artist.addAlbum(album)
+
+        // Get the album
+        val songAlbum = song.tags[Tag.Name.ALBUM]?.getOrDefault("Text", "N/A") ?: "N/A"
+        var album = artist.getAlbum(songAlbum)
+        if (album == null) {
+            album = Album(songAlbum)
+            artist.addAlbum(album)
+        }
+        album.addSong(song)
     }
 }
