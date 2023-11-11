b503952bc61afc08757c026f2e81f5c7cb0f1ded
Diego Vélez <diego.velez.dev@gmail.com>
2023-11-11 16:06:37 -0400

    Recursively check all files to see if they need transfer by using their sha1 hash.

--- src/main/kotlin/com/diego/velez/musickery/routes/TransferRoute.kt
+++ src/main/kotlin/com/diego/velez/musickery/routes/TransferRoute.kt
@@ -13,10 +13,14 @@ import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableSharedFlow
 import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
 
 // Flow that emits all the output from the terminal commands
 // It is sent to the client via SSE
 private val outputChannel = MutableSharedFlow<String>()
+
+private val USER_MUSIC = File("/storage/emulated/0/Music")
+private val skippedSongTransfersCount = AtomicInteger(0)
 
 /** Tendremos que usar 'adb' command en terminal para send and receive files while phone is connected to PC via USB
  * default android user dir: /storage/emulated/0
@@ -31,11 +35,11 @@ fun RootRoute.transferRoute() {
         }
 
         get("ls") {
-            val result = ls {
+            val filesInPhone = ls {
                 outputChannel.emit(it)
             }
 
-            if (result.output.isBlank()) {
+            if (filesInPhone.isEmpty()) {
                 outputChannel.emit("Folder is empty")
             }
 
@@ -43,13 +47,19 @@ fun RootRoute.transferRoute() {
         }
 
         get("to-device") {
-            val lsOutput = ls().output
-            val filesInPhone = lsOutput.split("\n")
+            skippedSongTransfersCount.set(0)
             Discography.getDefaultMusicFolder()
                 .listFiles()!!
-                .filterNotWithCallback(
-                    { filesInPhone.contains(it.name) },
-                    { outputChannel.emit("${it.name} already in phone, skipping.") })
+                .map { getAllFilesThatNeedTransfer(ls(), it) }
+                .flatMap { it.await() }
+                .also {
+                    if (it.isEmpty()) {
+                        outputChannel.emit("No songs needed to transfer.")
+                    } else {
+                        outputChannel.emit("Skipped ${skippedSongTransfersCount.get()} songs that were already in the phone.")
+                        outputChannel.emit("Transferring ${it.size} songs.")
+                    }
+                }
                 .map { processTransfer(it) }
                 .forEach { it.await() }
             call.respond(HttpStatusCode.OK)
@@ -76,35 +86,95 @@ fun RootRoute.transferRoute() {
 }
 
 /**
- * Same as [filterNot] but has a [callback] that is called when [predicate] is true.
- */
-private suspend fun <T> Array<out T>.filterNotWithCallback(
-    predicate: (T) -> Boolean,
-    callback: suspend (T) -> Unit
-): List<T> {
-    val destination = ArrayList<T>()
-    for (element in this) {
-        if (!predicate(element)) {
-            destination.add(element)
-        } else {
-            callback(element)
-        }
-    }
-    return destination
-}
-
-/**
- * Transfers [file] to the android device.
+ * Transfers a file on pc to the android device connected.
+ * @param file The [File] on pc to transfer.
  */
 private fun CoroutineScope.processTransfer(file: File): Deferred<Unit> = async {
-    val terminalCommand = listOf("adb", "push", file.absolutePath, "/storage/emulated/0/Music")
-    outputChannel.emit("Transferring ${file.absolutePath}")
+    val phonePath = if (file.parentFile == Discography.getDefaultMusicFolder()) {
+        USER_MUSIC.absolutePath
+    } else {
+        val relativePath = file.relativeTo(Discography.getDefaultMusicFolder()).parent
+        USER_MUSIC.resolve(relativePath).absolutePath
+    }
+    outputChannel.emit("Transferring $file to $phonePath")
+    val terminalCommand = listOf("adb", "push", file.absolutePath, phonePath)
     Terminal.run(terminalCommand) { output ->
         outputChannel.emit(output)
     }
 }
 
-suspend fun ls(callback: LineOutput = {}): Terminal.Result {
-    val terminalCommand = "adb shell ls -a /storage/emulated/0/Music".split(" ")
-    return Terminal.run(terminalCommand, callback)
+/**
+ * Gets all the files that need transfer within [fileInPc].
+ * @param filesInPhone A [List] of each file in the phone's folder that matches
+ * the relative location of [fileInPc].
+ * @param fileInPc The pc file to check if it is already in the phone.
+ * This should match the [filesInPhone]'s relative location on phone.
+ * @return A [List] of [File] where each file represents a file object on the pc
+ * that needs to be transferred to the phone.
+ */
+private fun CoroutineScope.getAllFilesThatNeedTransfer(
+    filesInPhone: List<String>,
+    fileInPc: File
+): Deferred<List<File>> = async {
+    if (!filesInPhone.contains(fileInPc.name)) {
+        return@async listOf(fileInPc)
+    }
+
+    // Recursively check all files/folders to see if they need to be transferred
+    if (fileInPc.isDirectory) {
+        val relativePath = fileInPc.toRelativeString(Discography.getDefaultMusicFolder())
+        return@async fileInPc
+            .listFiles()!!
+            .map { getAllFilesThatNeedTransfer(ls(relativePath), it) }
+            .flatMap { it.await() }
+    }
+
+    val pcFileShaResult = sha1sum(fileInPc.absolutePath)
+    val relativeSongPath = fileInPc.toRelativeString(Discography.getDefaultMusicFolder())
+    val phoneFileShaResult = sha1sum(USER_MUSIC.resolveWithQuotes(relativeSongPath), true)
+
+    // If the files hash does not match, then there is a tag difference, thus a transfer is needed.
+    if (pcFileShaResult != phoneFileShaResult) {
+        return@async listOf(fileInPc)
+    }
+
+    skippedSongTransfersCount.incrementAndGet()
+    return@async emptyList()
 }
+
+/**
+ * Returns a list where each item is a file in [USER_MUSIC]/[relativePath].
+ * @param relativePath Relative path to [USER_MUSIC] folder to ls into.
+ * Default is [USER_MUSIC] itself.
+ * @param callback [LineOutput] callback for [Terminal.run].
+ * @return [List] of each file.
+ */
+suspend fun ls(relativePath: String = "", callback: LineOutput = {}): List<String> {
+    val terminalCommand = listOf("adb", "shell", "ls", "-a", USER_MUSIC.resolveWithQuotes(relativePath))
+    return Terminal.run(terminalCommand, callback).output.split("\n")
+}
+
+/**
+ * Check the sha1 sum of a file at [path].
+ * Uses the `sha1sum` terminal command.
+ * @param path The absolute path of the file to check the sha1 sum of.
+ * @param onPhone Checks for the file at [path] in the phone using adb.
+ * @return The sha1 sum of the file at [path].
+ */
+suspend fun sha1sum(path: String, onPhone: Boolean = false): String {
+    val fileShaCommand = mutableListOf("sha1sum", path)
+
+    if (onPhone) {
+        fileShaCommand.add(0, "adb")
+        fileShaCommand.add(1, "shell")
+        println(fileShaCommand)
+    }
+
+    return Terminal.run(fileShaCommand).output.split(" ").first()
+}
+
+/**
+ * Surround [File.resolve] absolute path with quotes.
+ * Needed for when strings with spaces are used in terminal commands.
+ */
+private fun File.resolveWithQuotes(path: String): String = "\"${this.resolve(path).absolutePath}\""
